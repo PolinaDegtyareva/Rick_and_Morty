d5f3e306b51e204fef8ee791ce2a0ed8d985eb3d
Polina <polina.degtyareva.99@gmail.com>
2022-07-02 15:09:34 +0300

    added function getLocation

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ async function getAlive() {
 
 getAlive()
 
+async function getLocation() {
+    let response1 = await fetch ('https://rickandmortyapi.com/api/location/3')
+    let text1 = await response1.text()
+    console.log (text1)
+}
+
+getLocation()
